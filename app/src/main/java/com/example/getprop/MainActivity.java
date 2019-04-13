2e0936e239ac96b639c8151bc8b628aa92546a6c
taohongliang <taohongliang@meizu.com>
2019-04-13 18:20:03 +0800

    version 1.0
    1. 引入 Linkedmap 结构来顺序式保存需要获取到的属性和属性值。
    2. 成功将 Linkedmap 中的属性和属性值显示在了 textview 上。
    
    Todo:
    1. 需要把每个属性的属性值解释成大家都理解的意思。
    2. 尝试在 textview 中添加表格。
    3. 需要对 textview 中输出的内容进行格式化：对齐等。便于查看。

--- app/src/main/java/com/example/getprop/MainActivity.java
+++ app/src/main/java/com/example/getprop/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.getprop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,23 +24,44 @@ public class MainActivity extends AppCompatActivity {
         BufferedReader reader = null;
         String content = "";
 
+        String[] info = {
+                "ro.meizu.hardware.sn",     //SN
+                "ro.boot.psn",              //PSN
+                "ro.meizu.hardware.imei1",  //IMEI1
+                "ro.meizu.hardware.imei2",  //IMEI2
+                "ro.meizu.hardware.meid",   //MEID
+                "ro.build.inside.id",       //Flyme dailybuild 版本号
+                "ro.meizu.pcb.version",     //硬件版本：B0,B1,B2,NPI,MP
+                "ro.build.type",            //user or userdebug
+                "ro.secure",                //手机是否开启security 1:开启，0：未开启
+                "ro.meizu.bl_unlock",       //手机锁状态 1：unlock 0:lock
+                "ro.flyme.root.state",      //手机root状态 1：root 0:unroot
+        };
+
+        Map<String, String> map = new LinkedHashMap<>();
+
         try {
-            Process process = Runtime.getRuntime().exec("getprop ro.build.inside.id");
-            reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            for (int i = 0; i < info.length; i++) {
+                Process process = Runtime.getRuntime().exec("getprop " + info[i]);
+                reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-            StringBuffer output = new StringBuffer();
-            int read;
-            char[] buffer = new char[1024];
+                StringBuffer output = new StringBuffer();
+                char[] buffer = new char[1024];
+                int read;
 
-            while ((read = reader.read(buffer)) > 0 ) {
-                output.append(buffer, 0, read);
+                while ((read = reader.read(buffer)) > 0)
+                    output.append(buffer, 0, read);
+                reader.close();
+                content = output.toString();
+
+                map.put(info[i], content);
             }
-            reader.close();
-            content = output.toString();
-            Log.e("taohl", content); // 打印 content
 
-            taohltext.setText("dailybuild ver ： " + content);
-
+            // 把 map 的 key 和 value 都输出到所创建的 textview 上。
+            String buffer1 = "";
+            for (String key : map.keySet())
+                buffer1 = buffer1 + key + " = " + map.get(key);
+            taohltext.setText(buffer1);
         } catch (IOException e) {
                 e.printStackTrace();
         }
